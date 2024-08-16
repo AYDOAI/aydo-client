d9a3fc25690fc8f4f52b09cd442c1fc0f5ce1886
dvan <deewvn@gmail.com>
2024-08-17 00:10:02 +0300

    added back button

--- src/app/elements/header/header.component.ts
+++ src/app/elements/header/header.component.ts
@@ -11,6 +11,7 @@ export class HeaderComponent extends BaseElement {
 
   @Input() title = '';
   @Input() add: FrameStep = '';
+  @Input() back: string = '';
 
   menu = [
     {link:'/dashboard', icon: 'menu-unknown', title: 'Dashboard', step: 'dashboard'},
@@ -35,6 +36,10 @@ export class HeaderComponent extends BaseElement {
 
   addClick() {
     this.ui.goStep(this.add);
+  }
+
+  public backClick(): void {
+    this.router.navigate([this.back]);
   }
 
 }
