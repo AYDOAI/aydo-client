cac8c4b9f2590b0c84f520a98344b726553a244d
Denis Aydo <pythonist001@proton.me>
2024-11-20 20:31:40 +0300

    Add environment.development

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    main_url: 'http://192.168.1.14:3000',
+    log: {
+        backend: {
+            request: {
+                register: true
+            },
+            response: {
+                register: true
+            }
+        }
+    },
+};
