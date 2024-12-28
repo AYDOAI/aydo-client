c0533579f1222d451162588748658a3e4f094a8b
Denis Aydo <pythonist001@proton.me>
2024-12-28 12:21:35 +0300

    Add testing env

---
+++ src/environments/environment.testing.ts
@@ -0,0 +1,19 @@
+export const environment = {
+    production: false,
+    main_url: 'https://app.test.aydo.ai',
+    index_url: '/streams',
+    log: {
+        backend: {
+            request: {
+                register: true
+            },
+            response: {
+                register: true
+            }
+        }
+    },
+    recaptcha: {
+        enabled: true,
+        sitekey: '6LdtzYYqAAAAANtcIGe3hHHksnciNfFxHVKX7Y0r'
+    }
+};
