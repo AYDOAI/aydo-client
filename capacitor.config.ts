fff84462ca81d063c1252a58fd782330b5bb1e01
dvan <deewvn@gmail.com>
2025-01-21 17:40:04 +0300

    change appName

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.aydo.app',
-  appName: 'client',
+  appName: 'AYDO',
   webDir: 'www',
   cordova: {
     preferences: {
