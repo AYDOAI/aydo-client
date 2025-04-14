fdcf428a4773c56629417b416a2a53bc0fc305c8
Denis Aydo <pythonist001@proton.me>
2025-04-14 16:51:07 +0300

    #433 Описание устройства

--- src/app/models/gateway.model.ts
+++ src/app/models/gateway.model.ts
@@ -29,6 +29,7 @@ export class DriverItem extends BaseItem {
   className: string | undefined;
   parentClassName: string | undefined;
   name: string | undefined;
+  desciption: string | undefined;
   settings: SettingsModel | undefined;
 
   constructor(parent: BaseModel, data: any) {
--- src/environments/environment.testing.ts
+++ src/environments/environment.testing.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   main_url: 'https://cloud.test.aydo.ai',
   platform: (window as any)['__env']?.platform || 'web',
   index_url: '/dashboard',
