aade9e7e02f379f0a8fee01b2585bbd5fed66b98
dvan <deewvn@gmail.com>
2025-05-27 16:26:14 +0300

    #510

--- src/app/pages/streams/project/project.component.ts
+++ src/app/pages/streams/project/project.component.ts
@@ -129,6 +129,7 @@ export class ProjectComponent extends BaseComponent implements ViewWillEnter {
             };
             this.isStreamingActive = this.checkStreamingActive(dataStream);
             this.streamSubject.next(dataStream);
+            this.streamService.reloadData();
           }
         },
       });
