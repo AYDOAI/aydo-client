fad671e0cfd15802afc52f5688e9d52540354192
aleks <gesashka@gmail.com>
2025-06-05 15:56:35 +0400

    Update error handling in ws-request service and inject ErrorHandlerService in app component

--- src/app/services/ws-request.service.ts
+++ src/app/services/ws-request.service.ts
@@ -218,7 +218,7 @@ export class WsRequestService {
     }
 
     if (!ignoreError) {
-      this.errors.onError(error);
+      this.errors.showError(message);
       console.log(`${error.status} ${error.statusText} (${url})`);
     }
 
